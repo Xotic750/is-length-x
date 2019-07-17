95639989aa034f277db55088ba62fb3ace073ce1
Graham Fairweather <xotic750@gmail.com>
2019-07-17 20:33:10 +0200

    :bookmark: v3.0.10
    Auto build

--- dist/is-length-x.js
+++ dist/is-length-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T17:29:58.541Z",
+  "date": "2019-07-17T18:33:04.416Z",
   "describe": "",
   "description": "Checks if `value` is a valid array-like length.",
   "file": "is-length-x.js",
   "hash": "e908edd1e77811c7365a",
   "license": "MIT",
-  "version": "3.0.9"
+  "version": "3.0.10"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
