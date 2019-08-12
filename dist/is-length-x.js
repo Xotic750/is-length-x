27b332b95ef3486b76702a7c179d937f1d39c978
Graham Fairweather <xotic750@gmail.com>
2019-08-12 23:59:33 +0200

    :bookmark: v3.0.26
    Auto run

--- dist/is-length-x.js
+++ dist/is-length-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T21:59:28.189Z",
+  "date": "2019-08-12T21:59:26.807Z",
   "describe": "",
   "description": "Checks if `value` is a valid array-like length.",
   "file": "is-length-x.js",
